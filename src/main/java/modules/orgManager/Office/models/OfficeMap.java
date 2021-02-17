0a2a8bb0d6a4ad8f0fbb32ccdb69cd088a0c4f2e
mat <mat@forest.id.au>
2021-02-17 16:42:55 +1030

    Exercise 12b - Office & Staff Map

---
+++ src/main/java/modules/orgManager/Office/models/OfficeMap.java
@@ -0,0 +1,77 @@
+package modules.orgManager.Office.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.locationtech.jts.geom.Geometry;
+import org.skyve.metadata.view.model.map.MapFeature;
+import org.skyve.metadata.view.model.map.MapItem;
+import org.skyve.metadata.view.model.map.MapModel;
+import org.skyve.metadata.view.model.map.MapResult;
+
+import modules.orgManager.Staff.StaffExtension;
+import modules.orgManager.domain.Office;
+
+public class OfficeMap extends MapModel<Office> {
+
+	@Override
+	public MapResult getResult(Geometry mapBounds) throws Exception {
+
+		MapResult result = new MapResult();
+		List<MapItem> items = new ArrayList<>();
+		result.setItems(items);
+
+		Office office = getBean();
+		Geometry officeLocation = office.getLocation();
+
+		if (officeLocation != null) {
+
+//			result.setMapExtents(officeLocation.getEnvelopeInternal());
+
+			if (mapBounds.intersects(officeLocation)) {
+				MapItem item = new MapItem();
+				item.setBizId(office.getBizId());
+				item.setModuleName(office.getBizModule());
+				item.setDocumentName(office.getBizDocument());
+				item.setInfoMarkup(office.getBizKey());
+
+				MapFeature feature = new MapFeature();
+				feature.setGeometry(officeLocation);
+				feature.setFillColour("#FF7F00"); // orange
+				feature.setFillOpacity("0.15");
+				feature.setStrokeColour("#000000"); // black
+				item.getFeatures().add(feature);
+				items.add(item);
+
+				List<StaffExtension> employees = office.getEmployees();
+				for (StaffExtension staffMember : employees) {
+
+					MapItem newFeature = createStaffFeature(staffMember);
+					items.add(newFeature);
+				}
+			}
+		}
+
+		return result;
+	}
+
+	private MapItem createStaffFeature(StaffExtension staff) {
+
+		MapFeature feature = new MapFeature();
+		feature.setGeometry(staff.getLocation());
+
+		MapItem item = new MapItem();
+		item.getFeatures().add(feature);
+		item.setBizId(staff.getBizId());
+		item.setModuleName(staff.getBizModule());
+		item.setDocumentName(staff.getBizDocument());
+		item.setInfoMarkup(staff.getBizKey() + " TODO!");
+		
+		System.err.println(staff.getImage());
+
+		return item;
+	}
+	
+	
+
+}
